b8c375a9f6417eb197a267f83598495dfbe57cfe
Jintu Das <jintu@cogneotech.com>
2026-01-20 05:12:41 +0530

    chore: Bump app version to 1.0.11

--- app.config.js
+++ app.config.js
@@ -64,7 +64,7 @@ export default {
   expo: {
     name: "Troodie",
     slug: "troodie",
-    version: "1.0.10",
+    version: "1.0.11",
     orientation: "portrait",
     icon: "./assets/images/troodie_icon_logo.jpg",
     scheme: "troodie",
